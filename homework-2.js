77f9b007fbacc9aa610cb6f6696d4904e8abbf04
Dogukan Basaran <dogukanbasarann@hotmail.com>
2023-08-19 15:36:57 +0300

    NodeJS Homeworks 2 - Blog

--- homework-2.js
+++ homework-2.js
@@ -1,7 +1,7 @@
 /* 
 Post Sıralama ve Post Ekleme
 Blog oluşturmaya hazır mısınız? Konsol ekranında postlarımızı sıralayalım, 
-sonrasında yeni bir post oluşturalım ve yeni post ile birlikte postlarımızı tekrar sıralayalım.
+sonrasında yeni bir post oluşturalım ve yeni post ile birlikte postlarımızı tekrar sıralayalım..
 
 */  
 
