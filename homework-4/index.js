4f5657118f640878c7938e1922f405bb77e3f841
Dogukan Basaran <dogukanbasarann@hotmail.com>
2023-08-19 17:35:38 +0300

    NodeJS Homeworks 4 - FS kullanimi

---
+++ homework-4/index.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+
+
+
+// CREATE 
+// fs.writeFile("employees.json", 
+
+// '[{"name": "Employees 1 Name", "salary": 2000},', "utf-8", (err) => {
+
+//         if(err) console.log("HATA:" + err);      
+//         console.log("Dosya oluşturuldu.")
+//     }
+// )
+
+
+// READ
+// fs.readFile("employees.json", "utf-8",(err,data) =>{
+//     if(err) console.log("Dosya okunamadı : " + err)
+//     console.log(data);
+// })
+
+// UPDATE
+// fs.appendFile(
+//     "employees.json", '\n{"name": "Doğukan Başaran", "salary": 50000}]', "utf-8", (err) => {
+//         if(err) console.log("HATA:" + err);
+//         console.log("Dosya güncellendi.");
+//     }
+// )
+
+
+// DELETE
+
+// fs.unlink("employees.json", (err) => {
+//     if(err) console.log("HATA: " + err)
+//     console.log("Dosya silindi.");
+// })
